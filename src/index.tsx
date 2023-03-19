f13ab55eb0e5b469102d3d67219d22cd0edba7d5
JasonLe <Dung2781993@gmail.com>
2023-03-19 11:32:13 +1100

    Update title

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import { createRoot } from "react-dom/client";
 
 const App = () => {
-  return <div>Hello React</div>;
+  return <div>Testing</div>;
 };
 
 const container = document.getElementById("app");
